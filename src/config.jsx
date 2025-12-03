2b740186c43260ab966661958ca46d8a004aba50
Lab Grown Diamonds <labgrowndiamonds@Smits-MacBook-Pro.local>
2025-12-03 13:21:02 +0530

    changing new

--- src/config.jsx
+++ src/config.jsx
@@ -1,7 +1,3 @@
 // config.jsx
 export const BACKEND_APP_URL = import.meta.env.VITE_BACKEND_APP_URL;
-<<<<<<< HEAD
-export const IMG_PATH = import.meta.env.VITE_IMG_PATH;
-=======
 export const IMG_PATH = 'https://jewel.dnav360.com';
->>>>>>> d0b8ea1 (Merge remote ios with local ios)
